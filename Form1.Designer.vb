02f32a6e54711528de5706c8911790e6cac1d7b3
tundey08 <32247794+tundey08@users.noreply.github.com>
2024-09-14 14:54:48 -0400

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,165 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        lblTitle = New Label()
+        lblAuthor = New Label()
+        lblYear = New Label()
+        ListBoxBooks = New ListBox()
+        btnAdd = New Button()
+        btnView = New Button()
+        btnUpdate = New Button()
+        btnDelete = New Button()
+        txtTitle = New TextBox()
+        txtAuthor = New TextBox()
+        txtYear = New TextBox()
+        SuspendLayout()
+        ' 
+        ' lblTitle
+        ' 
+        lblTitle.AutoSize = True
+        lblTitle.Location = New Point(118, 55)
+        lblTitle.Name = "lblTitle"
+        lblTitle.Size = New Size(29, 15)
+        lblTitle.TabIndex = 0
+        lblTitle.Text = "Title"
+        ' 
+        ' lblAuthor
+        ' 
+        lblAuthor.AutoSize = True
+        lblAuthor.Location = New Point(118, 115)
+        lblAuthor.Name = "lblAuthor"
+        lblAuthor.Size = New Size(44, 15)
+        lblAuthor.TabIndex = 1
+        lblAuthor.Text = "Author"
+        ' 
+        ' lblYear
+        ' 
+        lblYear.AutoSize = True
+        lblYear.Location = New Point(118, 180)
+        lblYear.Name = "lblYear"
+        lblYear.Size = New Size(89, 15)
+        lblYear.TabIndex = 2
+        lblYear.Text = "PublicationYear"
+        ' 
+        ' ListBoxBooks
+        ' 
+        ListBoxBooks.FormattingEnabled = True
+        ListBoxBooks.ItemHeight = 15
+        ListBoxBooks.Location = New Point(198, 269)
+        ListBoxBooks.Name = "ListBoxBooks"
+        ListBoxBooks.Size = New Size(354, 169)
+        ListBoxBooks.TabIndex = 3
+        ' 
+        ' btnAdd
+        ' 
+        btnAdd.Location = New Point(168, 218)
+        btnAdd.Name = "btnAdd"
+        btnAdd.Size = New Size(75, 23)
+        btnAdd.TabIndex = 4
+        btnAdd.Text = "Add"
+        btnAdd.UseVisualStyleBackColor = True
+        ' 
+        ' btnView
+        ' 
+        btnView.Location = New Point(288, 217)
+        btnView.Name = "btnView"
+        btnView.Size = New Size(75, 23)
+        btnView.TabIndex = 5
+        btnView.Text = "View"
+        btnView.UseVisualStyleBackColor = True
+        ' 
+        ' btnUpdate
+        ' 
+        btnUpdate.Location = New Point(411, 216)
+        btnUpdate.Name = "btnUpdate"
+        btnUpdate.Size = New Size(75, 23)
+        btnUpdate.TabIndex = 6
+        btnUpdate.Text = "Update"
+        btnUpdate.UseVisualStyleBackColor = True
+        ' 
+        ' btnDelete
+        ' 
+        btnDelete.Location = New Point(536, 215)
+        btnDelete.Name = "btnDelete"
+        btnDelete.Size = New Size(75, 23)
+        btnDelete.TabIndex = 7
+        btnDelete.Text = "Delete"
+        btnDelete.UseVisualStyleBackColor = True
+        ' 
+        ' txtTitle
+        ' 
+        txtTitle.Location = New Point(245, 48)
+        txtTitle.Name = "txtTitle"
+        txtTitle.Size = New Size(276, 23)
+        txtTitle.TabIndex = 8
+        ' 
+        ' txtAuthor
+        ' 
+        txtAuthor.Location = New Point(245, 112)
+        txtAuthor.Name = "txtAuthor"
+        txtAuthor.Size = New Size(276, 23)
+        txtAuthor.TabIndex = 9
+        ' 
+        ' txtYear
+        ' 
+        txtYear.Location = New Point(245, 177)
+        txtYear.Name = "txtYear"
+        txtYear.Size = New Size(276, 23)
+        txtYear.TabIndex = 10
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = SystemColors.ActiveCaption
+        ClientSize = New Size(800, 450)
+        Controls.Add(txtYear)
+        Controls.Add(txtAuthor)
+        Controls.Add(txtTitle)
+        Controls.Add(btnDelete)
+        Controls.Add(btnUpdate)
+        Controls.Add(btnView)
+        Controls.Add(btnAdd)
+        Controls.Add(ListBoxBooks)
+        Controls.Add(lblYear)
+        Controls.Add(lblAuthor)
+        Controls.Add(lblTitle)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents lblTitle As Label
+    Friend WithEvents lblAuthor As Label
+    Friend WithEvents lblYear As Label
+    Friend WithEvents ListBoxBooks As ListBox
+    Friend WithEvents btnAdd As Button
+    Friend WithEvents btnView As Button
+    Friend WithEvents btnUpdate As Button
+    Friend WithEvents btnDelete As Button
+    Friend WithEvents txtTitle As TextBox
+    Friend WithEvents txtAuthor As TextBox
+    Friend WithEvents txtYear As TextBox
+End Class
---
+++ Form1.vb
@@ -0,0 +1,74 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Public Class Book
+        Public Property Title As String
+        Public Property Author As String
+        Public Property PublicationYear As Integer
+
+        Public Overrides Function ToString() As String
+            Return $"{Title} by {Author}, {PublicationYear}"
+        End Function
+    End Class
+
+    Private books As New List(Of Book)
+
+    Private Sub btnAdd_Click(sender As Object, e As EventArgs) Handles btnAdd.Click
+        If String.IsNullOrWhiteSpace(txtTitle.Text) OrElse String.IsNullOrWhiteSpace(txtAuthor.Text) OrElse String.IsNullOrWhiteSpace(txtYear.Text) Then
+            MessageBox.Show("Please fill in all fields.")
+            Return
+        End If
+
+        Dim newBook As New Book With {
+        .Title = txtTitle.Text,
+        .Author = txtAuthor.Text,
+        .PublicationYear = Convert.ToInt32(txtYear.Text)
+    }
+
+        books.Add(newBook)
+        UpdateBookList()
+        ClearInputFields()
+    End Sub
+
+    Private Sub UpdateBookList()
+        ListBoxBooks.DataSource = Nothing
+        ListBoxBooks.DataSource = books
+    End Sub
+
+    Private Sub btnUpdate_Click(sender As Object, e As EventArgs) Handles btnUpdate.Click
+        If ListBoxBooks.SelectedIndex <> -1 Then
+            Dim selectedBook As Book = CType(ListBoxBooks.SelectedItem, Book)
+            selectedBook.Title = txtTitle.Text
+            selectedBook.Author = txtAuthor.Text
+            selectedBook.PublicationYear = Convert.ToInt32(txtYear.Text)
+            UpdateBookList()
+        End If
+    End Sub
+
+    Private Sub btnDelete_Click(sender As Object, e As EventArgs) Handles btnDelete.Click
+        If ListBoxBooks.SelectedIndex <> -1 Then
+            books.RemoveAt(ListBoxBooks.SelectedIndex)
+            UpdateBookList()
+        End If
+    End Sub
+
+    Private Sub ListBoxBooks_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ListBoxBooks.SelectedIndexChanged
+
+        If ListBoxBooks.SelectedIndex <> -1 Then
+            Dim selectedBook As Book = CType(ListBoxBooks.SelectedItem, Book)
+            txtTitle.Text = selectedBook.Title
+            txtAuthor.Text = selectedBook.Author
+            txtYear.Text = selectedBook.PublicationYear.ToString()
+        End If
+    End Sub
+
+    Private Sub ClearInputFields()
+        txtTitle.Clear()
+        txtAuthor.Clear()
+        txtYear.Clear()
+    End Sub
+
+
+End Class
